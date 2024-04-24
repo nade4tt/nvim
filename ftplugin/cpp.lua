8a1a11e781924ee5f0c19de14afb267d346e5508
nade4tt <denisnarat8@gmail.com>
2024-04-24 14:37:40 +0200

    cpp compile command update for c++17

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>R", "<cmd>!clang++ --debug % -o out && ./out<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>R", "<cmd>!clang++ -std=c++17 --debug % -o out && ./out<CR>", { noremap = true })
