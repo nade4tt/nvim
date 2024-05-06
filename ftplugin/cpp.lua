5e29d76faa4a8443c55cc59211d239f364a43d19
nade4tt <denisnarat8@gmail.com>
2024-05-06 09:36:26 +0200

    Update cpp.lua
    c++ compile for windows

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,1 +1,5 @@
-vim.keymap.set("n", "<leader>R", "<cmd>!clang++ -std=c++17 --debug % -o out && ./out<CR>", { noremap = true })
+if vim.fn.has("win32") then
+	vim.keymap.set("n", "<leader>R", "<cmd>!g++ -std=c++17 --debug % -o out && out.exe<CR>", { noremap = true })
+else
+	vim.keymap.set("n", "<leader>R", "<cmd>!clang++ -std=c++17 --debug % -o out && ./out<CR>", { noremap = true })
+end
