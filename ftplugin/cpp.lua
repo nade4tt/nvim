d99f022b9ec36b7ff40469905524780a8708caaf
nade4tt <denisnarat8@gmail.com>
2024-05-27 20:34:53 +0200

    compile cpp update

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,11 +1,18 @@
-if vim.fn.has("win32") then
-  -- Compile (Fast)
-  vim.keymap.set("n", "<F8>", "<cmd>!clang++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -Wunused-variable % -o out.exe<CR>", { noremap = true })
-  -- Build (Slow but safe)
-  vim.keymap.set("n", "<F9>", "<cmd>!clang++ -std=c++17 -Wshadow -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG % -o out.exe<CR>", { noremap = true })
+if vim.fn.has("linux") then
+	-- Compile (Fast)
+	vim.keymap.set(
+		"n",
+		"<F8>",
+		"<cmd>!clang++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -Wunused-variable % -o out && ./out<CR>",
+		{ noremap = true }
+	)
+	-- Build (Slow but safe)
+	vim.keymap.set(
+		"n",
+		"<F9>",
+		"<cmd>!clang++ -std=c++17 -Wshadow -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG % -o out && ./out<CR>",
+		{ noremap = true }
+	)
 else
-  -- Compile (Fast)
-  vim.keymap.set("n", "<F8>", "<cmd>!clang++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result % -o out<CR>", { noremap = true })
-  -- Build (Slow but safe)
-  vim.keymap.set("n", "<F9>", "<cmd>!clang++ -std=c++17 -Wshadow -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG % -o out<CR>", { noremap = true })
+	-- print("TODO")
 end
