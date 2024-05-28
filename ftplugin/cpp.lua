44710684700b386d4c9c75a65fb2a5fd6be47114
nade4tt <denisnarat8@gmail.com>
2024-05-28 08:18:16 +0200

    ftplugin cpp update for windows

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,4 +1,6 @@
-if vim.fn.has("linux") then
+local uname = vim.loop.os_uname().sysname
+
+if uname == "Linux" then
 	-- Compile (Fast)
 	vim.keymap.set(
 		"n",
@@ -13,6 +15,22 @@ if vim.fn.has("linux") then
 		"<cmd>!clang++ -std=c++17 -Wshadow -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG % -o out && ./out<CR>",
 		{ noremap = true }
 	)
+elseif uname:match("Windows") then
+	-- Compile (Fast)
+	vim.keymap.set(
+		"n",
+		"<F8>",
+		"<cmd>!clang++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -Wunused-variable % -o out.exe && .\\out.exe<CR>",
+		{ noremap = true }
+	)
+	-- Build (Slow but safe)
+	vim.keymap.set(
+		"n",
+		"<F9>",
+		"<cmd>!clang++ -std=c++17 -Wshadow -Wall -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG % -o out.exe && .\\out.exe<CR>",
+		{ noremap = true }
+	)
+
 else
-	-- print("TODO")
+  print("Unsupported system: " .. uname)
 end
