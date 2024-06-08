509283a84fe91292991b182f549570686363b32c
nade4tt <denisnarat8@gmail.com>
2024-06-08 22:12:26 +0200

    cssls

--- lua/narat/plugins/lsp/mason.lua
+++ lua/narat/plugins/lsp/mason.lua
@@ -40,6 +40,7 @@ return {
 				"vimls",
 				"jsonls",
         "cmake",
+        "cssls"
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
