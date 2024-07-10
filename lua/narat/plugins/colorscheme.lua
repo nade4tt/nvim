f8267b7eb883e2b1cbe713ef41ce5b4bcbee256b
nade4tt <denisnarat8@gmail.com>
2024-07-10 20:05:57 +0200

    line number coloring update

--- lua/narat/plugins/colorscheme.lua
+++ lua/narat/plugins/colorscheme.lua
@@ -2,15 +2,16 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
-    vim.cmd [[colorscheme tokyonight]]
+    vim.cmd [[colorscheme tokyonight-day]]
     -- vim.cmd [[colorscheme catppuccin]]
     vim.opt.background = "dark"
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.g.tokyonight_dark_float = true
     vim.cmd "set guicursor=a:blinkon40"
     vim.cmd("highlight LineNr guifg=#ffffff")
+    vim.cmd("highlight LineNrAbove guifg=#ffffff")
+    vim.cmd("highlight LineNrBelow guifg=#ffffff")
     vim.cmd("highlight CursorLineNr guifg=#00ffff")
     vim.cmd("highlight Pmenu guibg=NONE")
   end,
