7c10a4f6c610a9157180e9ebcd5de5c90eb0de60
nade4tt <denisnarat8@gmail.com>
2024-09-26 09:29:16 +0200

    sl langauge removed

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,7 +2,7 @@ vim.opt.linebreak = true
 vim.opt.wrap = false
 
 vim.wo.spell = true
-vim.opt.spelllang = { "en_us", "sl" }
+vim.opt.spelllang = { "en_us" }
 
 -- vim.opt.fo = "want"
 vim.opt.tw = 80
