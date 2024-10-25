bab8fa705106b49493dfcdb3ef802b4b85c0b2da
nade4tt <denisnarat8@gmail.com>
2024-10-25 16:11:52 +0200

    leap, update

--- lua/narat/plugins/leap.lua
+++ lua/narat/plugins/leap.lua
@@ -7,12 +7,18 @@ return {
 		vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
 		vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
 
-    -- Use `cl` instead of `s` to avoid conflicts with `s` in normal mode
+		-- Use `cl` instead of `s` to avoid conflicts with `s` in normal mode
 		vim.keymap.set("n", "cl", "s", { noremap = true, silent = true })
 
+		-- Disable auto-jumping to the first match
 		leap.opts.safe_labels = {}
+
+		-- Disable previewing labels (labels preview as soon as you press the first character)
 		leap.opts.preview_filter = function()
 			return false
 		end
+
+    -- Disable graying out the search area
+    vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'None' })
 	end,
 }
