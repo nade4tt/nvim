9e22beef9c44c68763ed932cbd167f03418b9870
nade4tt <denisnarat8@gmail.com>
2024-12-15 23:50:43 +0100

    feat: adds terminal function for funnig AVL tests:

--- lua/narat/core/functions.lua
+++ lua/narat/core/functions.lua
@@ -1,64 +1,85 @@
 -- Toggle copilot
 local copilot_on = true
 vim.api.nvim_create_user_command("CopilotToggle", function()
-  if copilot_on then
-    vim.cmd("Copilot disable")
-    print("Copilot OFF")
-  else
-    vim.cmd("Copilot enable")
-    print("Copilot ON")
-  end
-  copilot_on = not copilot_on
+	if copilot_on then
+		vim.cmd("Copilot disable")
+		print("Copilot OFF")
+	else
+		vim.cmd("Copilot enable")
+		print("Copilot ON")
+	end
+	copilot_on = not copilot_on
 end, { nargs = 0 })
 
 -- Search without jumping
 function SearchWithoutJump()
-  local pattern = vim.fn.input("Search: ")
-  vim.fn.setreg("/", pattern)
-  vim.cmd("set hlsearch")
+	local pattern = vim.fn.input("Search: ")
+	vim.fn.setreg("/", pattern)
+	vim.cmd("set hlsearch")
 end
 
 -- Print current range
 function PrintRange()
-  local range = vim.fn.getpos("'<") .. "," .. vim.fn.getpos("'>")
-  print(range)
+	local range = vim.fn.getpos("'<") .. "," .. vim.fn.getpos("'>")
+	print(range)
 end
 
 -- Fix sibaus auto formatting
 function FixSibau()
-  -- Brackets
-  vim.cmd([[%g/(\n/norm Jx]])
-  -- Commas
-  vim.cmd([[%g/,\n/norm Jx]])
-  -- Square brackets
-  vim.cmd([[%g/\[\n/norm Jx]])
+	-- Brackets
+	vim.cmd([[%g/(\n/norm Jx]])
+	-- Commas
+	vim.cmd([[%g/,\n/norm Jx]])
+	-- Square brackets
+	vim.cmd([[%g/\[\n/norm Jx]])
 end
 
 -- Telescope current buffer grep
 function TelescopeCurrentBufferGrep()
-  require("telescope.builtin").live_grep({
-    search_dirs = { vim.fn.expand("%:p") },
-  })
+	require("telescope.builtin").live_grep({
+		search_dirs = { vim.fn.expand("%:p") },
+	})
 end
 
 -- Telescope grep for files
 function TelescopeGrepFiles()
-  require("telescope.builtin").live_grep({ additional_args = { "-l" } })
+	require("telescope.builtin").live_grep({ additional_args = { "-l" } })
 end
 
 -- Terminal
 local job_id = 0
+
 vim.keymap.set("n", "<leader>st", function()
-  local current_file = vim.fn.expand("%:p")
-  local file_extension = vim.fn.expand("%:e")
+	local file_path = vim.fn.expand("%:p")
+	local file_extension = vim.fn.expand("%:e")
 
-  vim.cmd.vnew()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 10)
-  vim.cmd.term()
-  job_id = vim.bo.channel
+	vim.cmd.vnew()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 10)
+	vim.cmd.term()
+	job_id = vim.bo.channel
 
-  if file_extension == "py" then
-    vim.fn.chansend(job_id, "python " .. current_file .. "\n")
-  end
+	if file_extension == "py" then
+		vim.fn.chansend(job_id, "python " .. file_path .. "\n")
+	end
+end)
+
+-- For runnign AVL tests
+vim.keymap.set("n", "<leader>rr", function()
+	local filename = vim.fn.expand("%:t")
+
+	vim.ui.input({ prompt = "Test Execution: " }, function(input)
+		vim.cmd.vnew()
+		vim.cmd.wincmd("L")
+		vim.cmd.term()
+		job_id = vim.bo.channel
+
+		vim.fn.chansend(job_id, "powershell" .. "\n")
+
+		if input then
+			vim.fn.chansend(job_id, "rr " .. filename .. " " .. input .. "\n")
+		else
+			vim.fn.chansend(job_id, "rr " .. filename .. "\n")
+		end
+	end)
 end)
