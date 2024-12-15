2b9a747fe7db5888302045688ec00791ea233fd7
nade4tt <denisnarat8@gmail.com>
2024-12-15 23:00:08 +0100

    feat: maps terminal to <leader>st, updates leap to jump to the first match

--- lua/narat/plugins/leap.lua
+++ lua/narat/plugins/leap.lua
@@ -11,14 +11,14 @@ return {
 		vim.keymap.set("n", "cl", "s", { noremap = true, silent = true })
 
 		-- Disable auto-jumping to the first match
-		leap.opts.safe_labels = {}
+		-- leap.opts.safe_labels = {}
 
 		-- Disable previewing labels (labels preview as soon as you press the first character)
 		leap.opts.preview_filter = function()
 			return false
 		end
 
-    -- Disable graying out the search area
-    vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'None' })
+		-- Disable graying out the search area
+		vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'None' })
 	end,
 }
