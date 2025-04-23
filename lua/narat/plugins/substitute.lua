684fab2dba31c009fd3f4cd753e6723ae6b50ed6
nade4tt <denisnarat8@gmail.com>
2025-04-23 20:52:20 +0200

    Disables substitute, enables leap, removes unused markdown snippets

--- lua/narat/plugins/substitute.lua
+++ lua/narat/plugins/substitute.lua
@@ -1,14 +1,14 @@
 return {
-	"gbprod/substitute.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local substitute = require("substitute")
-		substitute.setup()
+	-- "gbprod/substitute.nvim",
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- config = function()
+	-- 	local substitute = require("substitute")
+	-- 	substitute.setup()
 
-		local keymap = require("narat.core.utils").keymap
-		keymap("n", "s", substitute.operator, { desc = "Substitute with motion" })
-		keymap("n", "ss", substitute.line, { desc = "Substitute line" })
-		keymap("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-		keymap("x", "s", substitute.visual, { desc = "Substitute visual selection" })
-	end,
+	-- 	local keymap = require("narat.core.utils").keymap
+	-- 	keymap("n", "s", substitute.operator, { desc = "Substitute with motion" })
+	-- 	keymap("n", "ss", substitute.line, { desc = "Substitute line" })
+	-- 	keymap("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+	-- 	keymap("x", "s", substitute.visual, { desc = "Substitute visual selection" })
+	-- end,
 }
