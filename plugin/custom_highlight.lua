67c8429c309b51f3d7f3623c1421eb8c697e3309
nade4tt <denisnarat8@gmail.com>
2025-04-30 19:00:36 +0200

    feat: spell, comments highlihght plugin

--- plugin/custom_highlight.lua
+++ plugin/custom_highlight.lua
@@ -2,16 +2,16 @@ vim.api.nvim_set_hl(0, "DEBUG", { fg = "#FFFFFF", bg = "#FF0000", bold = true })
 vim.api.nvim_set_hl(0, "DEBUG", { fg = "#FFFFFF", bg = "#FF0000", bold = true })
 vim.api.nvim_set_hl(0, "INFO", { fg = "#000000", bg = "#E6E600", bold = true })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = "*",
-	callback = function()
-		vim.fn.matchadd("DEBUG", ".*breakpoint().*", 1)
-		vim.fn.matchadd("DEBUG", '.*print("breakpoint").*', 1)
-		vim.fn.matchadd("DEBUG", ".*COF.*False.*", 1)
-		vim.fn.matchadd("DEBUG", ".*am.home*", 1)
-		vim.fn.matchadd("DEBUG", ".*am.topology*", 1)
-		vim.fn.matchadd("DEBUG", ".*app_manip*", 1)
-		vim.fn.matchadd("DEBUG", ".*app_manip as am*", 1)
-		vim.fn.matchadd("INFO", [[\v.*test_CMTEST_\d+.*\n?.*line \d+, in \w+]], 1)
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		vim.fn.matchadd("DEBUG", ".*breakpoint().*", 1)
+-- 		vim.fn.matchadd("DEBUG", '.*print("breakpoint").*', 1)
+-- 		vim.fn.matchadd("DEBUG", ".*COF.*False.*", 1)
+-- 		vim.fn.matchadd("DEBUG", ".*am.home*", 1)
+-- 		vim.fn.matchadd("DEBUG", ".*am.topology*", 1)
+-- 		vim.fn.matchadd("DEBUG", ".*app_manip*", 1)
+-- 		vim.fn.matchadd("DEBUG", ".*app_manip as am*", 1)
+-- 		vim.fn.matchadd("INFO", [[\v.*test_CMTEST_\d+.*\n?.*line \d+, in \w+]], 1)
+-- 	end,
+-- })
