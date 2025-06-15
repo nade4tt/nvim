c4c61aaff839649c1d8dd223c944495c344f4e85
nade4tt <denisnarat8@gmail.com>
2025-06-15 15:33:39 +0200

    feat: Adds new function AvlHighlightPtest

--- plugin/custom_highlight.lua
+++ plugin/custom_highlight.lua
@@ -2,16 +2,31 @@ vim.api.nvim_set_hl(0, "DEBUG", { fg = "#FFFFFF", bg = "#FF0000", bold = true })
 vim.api.nvim_set_hl(0, "DEBUG", { fg = "#FFFFFF", bg = "#FF0000", bold = true })
 vim.api.nvim_set_hl(0, "INFO", { fg = "#000000", bg = "#E6E600", bold = true })
 
--- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
--- 	pattern = "*",
--- 	callback = function()
--- 		vim.fn.matchadd("DEBUG", ".*breakpoint().*", 1)
--- 		vim.fn.matchadd("DEBUG", '.*print("breakpoint").*', 1)
--- 		vim.fn.matchadd("DEBUG", ".*COF.*False.*", 1)
--- 		vim.fn.matchadd("DEBUG", ".*am.home*", 1)
--- 		vim.fn.matchadd("DEBUG", ".*am.topology*", 1)
--- 		vim.fn.matchadd("DEBUG", ".*app_manip*", 1)
--- 		vim.fn.matchadd("DEBUG", ".*app_manip as am*", 1)
--- 		vim.fn.matchadd("INFO", [[\v.*test_CMTEST_\d+.*\n?.*line \d+, in \w+]], 1)
--- 	end,
--- })
+local highlight_on = false
+
+-- create a new function
+local function avl_highlight()
+	if highlight_on then
+		vim.fn.matchdelete(1001)
+		vim.fn.matchdelete(1002)
+		vim.fn.matchdelete(1003)
+		vim.fn.matchdelete(1004)
+		vim.fn.matchdelete(1005)
+		vim.fn.matchdelete(1006)
+		vim.fn.matchdelete(1007)
+		vim.fn.matchdelete(1008)
+		highlight_on = false
+	else
+		vim.fn.matchadd("DEBUG", ".*breakpoint().*", 1, 1001)
+		vim.fn.matchadd("DEBUG", '.*print("breakpoint").*', 1, 1002)
+		vim.fn.matchadd("DEBUG", ".*COF.*False.*", 1, 1003)
+		vim.fn.matchadd("DEBUG", ".*am.home*", 1, 1004)
+		vim.fn.matchadd("DEBUG", ".*am.topology*", 1, 1005)
+		vim.fn.matchadd("DEBUG", ".*app_manip*", 1, 1006)
+		vim.fn.matchadd("DEBUG", ".*app_manip as am*", 1, 1007)
+		vim.fn.matchadd("INFO", [[\v.*test_CMTEST_\d+.*\n?.*line \d+, in \w+]], 1, 1008)
+		highlight_on = true
+	end
+end
+
+vim.api.nvim_create_user_command("AvlHighlightPtest", avl_highlight, {})
