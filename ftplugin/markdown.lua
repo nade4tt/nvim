579730156b975721725abbd5412c12abc221cb2e
nade4tt <denisnarat8@gmail.com>
2025-07-10 13:42:19 +0200

    feat: adds markdown_render

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -8,12 +8,12 @@ vim.opt.fo = "want"
 vim.opt.textwidth = 80
 
 -- Hide markups
-vim.opt.conceallevel = 3
+vim.opt.conceallevel = 1
 vim.opt.concealcursor = "" -- Conceals in normal and command-line modes
 
 local keymap = require("narat.core.utils").keymap
 
-keymap("n", "<F12>", function()
-	local conceal_level = vim.api.nvim_get_option_value("conceallevel", {})
-	vim.opt.conceallevel = conceal_level == 0 and 3 or 0
-end)
+-- keymap("n", "<F12>", function()
+-- 	local conceal_level = vim.api.nvim_get_option_value("conceallevel", {})
+-- 	vim.opt.conceallevel = conceal_level == 0 and 3 or 0
+-- end)
