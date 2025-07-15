b7cfa5da64b3d4e8b7fd3d8b8548def7e4eca26e
nade4tt <denisnarat8@gmail.com>
2025-07-15 13:41:25 +0200

    feat: adds save_to_avl_pc

---
+++ plugin/save_to_avl_pc.lua
@@ -0,0 +1,9 @@
+local function save_to_avl_pc(opts)
+	local src_file = vim.fn.expand("%:p")
+	local dst_file = src_file:gsub("^/home/narat/avl/", "/mnt/c/github/")
+	vim.cmd("w! " .. dst_file)
+	print("Saved to: " .. dst_file)
+end
+
+vim.api.nvim_create_user_command("SaveToAvlPC", save_to_avl_pc, {})
+require("narat.core.utils").keymap({ "n", "t" }, "<F9>", save_to_avl_pc)
