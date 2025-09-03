d5a0e7725e25e48bd94f9efc8df78350639010ea
nade4tt <denisnarat8@gmail.com>
2025-09-03 23:57:56 +0200

    feat: removes harpoon, adds arrow

--- lua/narat/plugins/harpoon.lua
+++ lua/narat/plugins/harpoon.lua
@@ -1,31 +1,31 @@
 return {
-	"theprimeagen/harpoon",
+	-- "theprimeagen/harpoon",
 
-	config = function()
-		require("harpoon").setup({
-			menu = {
-				width = vim.api.nvim_win_get_width(0) - 4,
-			},
-		})
+	-- config = function()
+	-- 	require("harpoon").setup({
+	-- 		menu = {
+	-- 			width = vim.api.nvim_win_get_width(0) - 4,
+	-- 		},
+	-- 	})
 
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
-		local keymap = require("narat.core.utils").keymap
+	-- 	local mark = require("harpoon.mark")
+	-- 	local ui = require("harpoon.ui")
+	-- 	local keymap = require("narat.core.utils").keymap
 
-		keymap("n", "<leader>a", mark.add_file)
-		keymap("n", "<leader>w", ui.toggle_quick_menu)
+	-- 	keymap("n", "<leader>a", mark.add_file)
+	-- 	keymap("n", "<leader>w", ui.toggle_quick_menu)
 
-		keymap("n", "<C-h>", function()
-			ui.nav_file(1)
-		end)
-		keymap("n", "<C-t>", function()
-			ui.nav_file(2)
-		end)
-		keymap("n", "<C-n>", function()
-			ui.nav_file(3)
-		end)
-		keymap("n", "<C-s>", function()
-			ui.nav_file(4)
-		end)
-	end,
+	-- 	keymap("n", "<C-h>", function()
+	-- 		ui.nav_file(1)
+	-- 	end)
+	-- 	keymap("n", "<C-t>", function()
+	-- 		ui.nav_file(2)
+	-- 	end)
+	-- 	keymap("n", "<C-n>", function()
+	-- 		ui.nav_file(3)
+	-- 	end)
+	-- 	keymap("n", "<C-s>", function()
+	-- 		ui.nav_file(4)
+	-- 	end)
+	-- end,
 }
