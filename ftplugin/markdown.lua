531af2c26faaa8f18db556e342fe4b8e127f911d
nade4tt <denisnarat8@gmail.com>
2025-09-11 14:37:11 +0200

    feat: markdown conceal update

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -8,12 +8,18 @@ vim.opt.fo = "want"
 vim.opt.textwidth = 80
 
 -- Hide markups
-vim.opt.conceallevel = 1
-vim.opt.concealcursor = "" -- Conceals in normal and command-line modes
+vim.opt.conceallevel = 3
+vim.opt.concealcursor = "nc"
 
 local keymap = require("narat.core.utils").keymap
 
--- keymap("n", "<F12>", function()
--- 	local conceal_level = vim.api.nvim_get_option_value("conceallevel", {})
--- 	vim.opt.conceallevel = conceal_level == 0 and 3 or 0
--- end)
+keymap("n", "<F12>", function()
+	local conceal_level = vim.api.nvim_get_option_value("conceallevel", {})
+	if conceal_level == 0 then
+		vim.opt.conceallevel = 3
+		vim.opt.concealcursor = "nc"
+	else
+		vim.opt.conceallevel = 0
+		vim.opt.concealcursor = ""
+	end
+end)
--- lua/narat/plugins/arrow.lua
+++ lua/narat/plugins/arrow.lua
@@ -7,7 +7,7 @@ return {
 	},
 	opts = {
 		show_icons = true,
-		leader_key = ";", -- Recommended to be a single key
-		buffer_leader_key = "m", -- Per Buffer Mappings
+		leader_key = "<leader>w",
+		buffer_leader_key = "<leader>m", -- Per Buffer Mappings
 	},
 }
