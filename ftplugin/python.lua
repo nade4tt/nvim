4cf7ec5f8ea7a66e751e5b94e96880bec0da465b
nade4tt <denisnarat8@gmail.com>
2026-06-24 14:18:14 +0200

    fix: python iron and surround

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -18,71 +18,32 @@ vim.opt.textwidth = 0
 
 vim.opt.formatoptions:remove({ "t", "c", "a" })
 
-local function save_to_avl_pc()
-	local src_file = vim.fn.expand("%:p")
-	local dst_file = src_file:gsub("^/home/narat/avl/", "/mnt/c/github/")
-	vim.cmd("w! " .. dst_file)
-	print("Saved to: " .. dst_file)
-end
+-- local function save_to_avl_pc()
+-- 	local src_file = vim.fn.expand("%:p")
+-- 	local dst_file = src_file:gsub("^/home/narat/avl/", "/mnt/c/github/")
+-- 	vim.cmd("w! " .. dst_file)
+-- 	print("Saved to: " .. dst_file)
+-- end
 
-keymap({ "n", "t" }, "<F9>", save_to_avl_pc)
+-- keymap({ "n", "t" }, "<F9>", save_to_avl_pc)
 
--- iron.nvim REPL (Python only)
-local view = require("iron.view")
-local common = require("iron.fts.common")
+-- Run current Python file in a terminal (like Rust's cargo run)
+local function run_python()
+	local python = require("utils").get_python_path() or "python3"
+	local file = vim.fn.expand("%:p")
 
-require("iron").setup({
-	config = {
-		scratch_repl = true,
-		repl_definition = {
-			sh = {
-				command = { "zsh" },
-			},
-			python = {
-				command = function()
-					local venv = os.getenv("VIRTUAL_ENV")
-					if venv then
-						return { "ptpython" }
-					else
-						return { "ptpython" }
-					end
-				end,
-				format = common.bracketed_paste_python,
-				block_dividers = { "# %%", "#%%" },
-				env = { PYTHON_BASIC_REPL = "1" }, -- needed for python3.13+
-			},
-		},
-		repl_filetype = function(bufnr, ft)
-			return ft
+	if file == "" then
+		vim.notify("No file to run", vim.log.levels.WARN)
+		return
+	end
+
+	vim.api.nvim_create_autocmd("TermOpen", {
+		once = true,
+		callback = function()
+			vim.cmd("normal! G")
 		end,
-		dap_integration = true,
-		repl_open_cmd = {
-			view.split.vertical.rightbelow("%40"), -- cmd_1: open a repl to the right
-			view.split.rightbelow("%25"),          -- cmd_2: open a repl below
-		},
-	},
-	keymaps = {
-		toggle_repl = "<space>rr",
-		restart_repl = "<space>RR",
-		send_motion = "<space>sc",
-		visual_send = "<space>sc",
-		send_file = "<space>sf",
-		send_line = "<space>sl",
-		send_paragraph = "<space>sp",
-		send_until_cursor = "<space>su",
-		send_mark = "<space>sm",
-		send_code_block = "<space>sb",
-		send_code_block_and_move = "<space>sn",
-		mark_motion = "<space>mc",
-		mark_visual = "<space>mc",
-		remove_mark = "<space>md",
-		cr = "<space>s<cr>",
-		interrupt = "<space>s<space>",
-		exit = "<space>sq",
-		clear = "<space>cl",
-	},
-	highlight = {
-		italic = true,
-	},
-	ignore_blank_lines = true,
-})
+	})
+
+	vim.cmd("botright split | resize 30 | terminal " .. python .. " " .. vim.fn.shellescape(file))
+end
+keymap({ "n", "t" }, "<F9>", run_python)
--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,22 +1,19 @@
+local keymap = require("utils").keymap
+
 vim.pack.add({
 	{ src = "https://github.com/kylechui/nvim-surround", name = "surround", version = vim.version.range("4.x") },
 })
 
--- Disable default normal/visual keymaps so we can remap them
-vim.g.nvim_surround_no_normal_mappings = true
-vim.g.nvim_surround_no_visual_mappings = true
-
 require("nvim-surround").setup()
 
--- Keymaps (v4 uses <Plug> mappings, not setup keymaps table)
---
--- cs{motion}{char}  → add surround around a motion  (instead of default ys)
--- ds{char}          → delete surround                (same as default)
--- cS{old}{new}      → change surround                (was cs, now cS)
--- S{char}           → add surround in visual mode    (same as default)
+-- ys{motion}{char}  → add surround around a motion
+-- yss               → add surround around current line
+-- cs{old}{new}      → change surrounding pair
+-- ds{char}          → delete surrounding pair
+-- S{char}           → add surround in visual mode
 
-vim.keymap.set("n", "cs", "<Plug>(nvim-surround-normal)", { desc = "Add surround around motion" })
-vim.keymap.set("n", "css", "<Plug>(nvim-surround-normal-cur)", { desc = "Add surround around current line" })
-vim.keymap.set("n", "cS", "<Plug>(nvim-surround-change)", { desc = "Change surrounding pair" })
-vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)", { desc = "Delete surrounding pair" })
-vim.keymap.set("x", "S", "<Plug>(nvim-surround-visual)", { desc = "Surround visual selection" })
+keymap("n", "ys", "<Plug>(nvim-surround-normal)", { desc = "Add surround around motion" })
+keymap("n", "yss", "<Plug>(nvim-surround-normal-cur)", { desc = "Add surround around current line" })
+keymap("n", "cs", "<Plug>(nvim-surround-change)", { desc = "Change surrounding pair" })
+keymap("n", "ds", "<Plug>(nvim-surround-delete)", { desc = "Delete surrounding pair" })
+keymap("x", "S", "<Plug>(nvim-surround-visual)", { desc = "Surround visual selection" })
